e61042e0e8089a65747a894403aecbecca7f43d1
prashant6311 <prashantppatil761@gmail.com>
2022-10-14 11:41:58 +0530

    Employee TotalWorking Hours And Day Reached

--- employeeWage/EmployeeWage.java
+++ employeeWage/EmployeeWage.java
@@ -2,6 +2,7 @@ package employeeWage;
 
 public class EmployeeWage {
 
+	static final int TOTAL_WORKING_HOURS = 100;
 	static final int WORKING_DAYS_PER_M0NTH = 20;
 	static final int WAGE_PER_HOUR = 20;
 	static final int FULL_TIME_HOUR = 8;
@@ -12,29 +13,39 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 
 		System.out.println("welcome to Employee Wage");
-
 		int day = 1;
 		while (day <= WORKING_DAYS_PER_M0NTH) {
-			int dailywage = 0;
-			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+			int totalworkinghours1 = 0;
+			int totalwage1 = 0;
+			while (day <= WORKING_DAYS_PER_M0NTH && totalworkinghours1 < TOTAL_WORKING_HOURS) {
+				int dailywage = 0;
+				int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 
-			switch (empcheck) {
-			case IS_FULL_TIME:
-				System.out.println("Employee Full Time");
-				dailywage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-				break;
-			case IS_PART_TIME:
-				System.out.println("Employee Part Time");
-				dailywage = WAGE_PER_HOUR * PART_TIME_HOUR;
-				break;
+				switch (empcheck) {
+				case IS_FULL_TIME:
+					System.out.println("Employee Full Time");
+					dailywage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+					totalworkinghours1 = totalworkinghours1 + FULL_TIME_HOUR;
+					break;
+				case IS_PART_TIME:
+					System.out.println("Employee Part Time");
+					dailywage = WAGE_PER_HOUR * PART_TIME_HOUR;
+					totalworkinghours1 = totalworkinghours1 + PART_TIME_HOUR;
 
-			default:
-				System.out.println("Employee is Absent");
+					break;
+
+				default:
+					System.out.println("Employee is Absent");
+
+				}
+
+				totalwage1 = totalwage1 + dailywage;
+				System.out.println("Daly Wage => " + dailywage);
+				day++;
 			}
-
-			System.out.println("Daly Wage => " + dailywage);
-			day++;
+		
+		System.out.println("Total wage=>" + totalwage1);
+		System.out.println("Total working hours=>" + totalworkinghours1);
 		}
-
 	}
 }
