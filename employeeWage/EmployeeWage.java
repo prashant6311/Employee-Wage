91aacec3a2df2efe4fbf48e8ab6306792d44c958
prashant6311 <prashantppatil761@gmail.com>
2022-10-14 10:48:55 +0530

    Check Employee Daily Wage

--- employeeWage/EmployeeWage.java
+++ employeeWage/EmployeeWage.java
@@ -2,15 +2,26 @@ package employeeWage;
 
 public class EmployeeWage {
 
+	static final int WAGE_PER_HOUR = 20;
+	static final int FULL_TIME_HOUR = 8;
+
 	public static void main(String[] args) {
 
-		System.out.println("welcome to Employee Wage Computation Program");
+		System.out.println("welcome to Employee Wage");
 
-		int empcheck = (int)Math.floor(Math.random()*10)%2;
-		if(empcheck == 1)
-			System.out.println("Employee is Present");
-		else
-			System.out.println("Employee is Absent");
+		int dailywage = 0;
+		int empcheck = (int) Math.floor(Math.random() * 10) % 2;
+		if (empcheck == 1)
+			if (empcheck == 1) {
+				System.out.println("Employee is Present");
+
+				dailywage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+			} else {
+				System.out.println("Employee is Absent");
+
+			}
+		System.out.println("Daly Wage =>" + dailywage);
+
 	}
 
 }
