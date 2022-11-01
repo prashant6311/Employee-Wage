b1f8f53464c24bb66d898363cada4887cefb21c5
prashant6311 <prashantppatil761@gmail.com>
2022-11-01 20:19:08 +0530

    multiple company for emp wage

--- employeeWage/EmployeeWage.java
+++ employeeWage/EmployeeWage.java
@@ -2,55 +2,74 @@ package employeeWage;
 
 public class EmployeeWage {
 
-	static final int TOTAL_WORKING_HOURS = 100;
-	static final int WORKING_DAYS_PER_M0NTH = 20;
-	static final int WAGE_PER_HOUR = 20;
 	static final int FULL_TIME_HOUR = 8;
 	static final int PART_TIME_HOUR = 4;
 	static final int IS_FULL_TIME = 2;
 	static final int IS_PART_TIME = 1;
 
-	public static void main(String[] args) {
+	private String COMPONY_NAME;
+	private final int TOTAL_WORKING_HOURS;
+	private final int WORKING_DAYS_PER_MONTH;
+	private final int WAGE_PER_HOUR;
+	private int total_wage;
 
-		System.out.println("welcome to Employee Wage");
+	public EmployeeWage(String COMPANY_NAME, int WAGE_PER_HOUR, int WORKING_DAYS_PER_MONTH, int TOTAL_WORKING_HOURS) {
+		this.COMPONY_NAME = COMPONY_NAME;
+		this.WAGE_PER_HOUR = WAGE_PER_HOUR;
+		this.WORKING_DAYS_PER_MONTH = WORKING_DAYS_PER_MONTH;
+		this.TOTAL_WORKING_HOURS = TOTAL_WORKING_HOURS;
 
-		computeEmpWage();
 	}
 
-	public static void computeEmpWage() {
+	public int computeEmpWage() {
 		int day = 1;
-		while (day <= WORKING_DAYS_PER_M0NTH) {
-			int totalworkinghours1 = 0;
-			int totalwage1 = 0;
-			while (day <= WORKING_DAYS_PER_M0NTH && totalworkinghours1 < TOTAL_WORKING_HOURS) {
-				int dailywage = 0;
-				int empcheck = (int) Math.floor(Math.random() * 10) % 3;
+		int totalworkinghours = 0;
+		int tatolwage = 0;
+		while (day <= WORKING_DAYS_PER_MONTH && totalworkinghours <= TOTAL_WORKING_HOURS) {
+			int dailywage = 0;
+			int empcheck = (int) Math.floor(Math.random() * 10) % 3;
 
-				switch (empcheck) {
-				case IS_FULL_TIME:
-					System.out.println("Employee Full Time");
-					dailywage = WAGE_PER_HOUR * FULL_TIME_HOUR;
-					totalworkinghours1 = totalworkinghours1 + FULL_TIME_HOUR;
-					break;
-				case IS_PART_TIME:
-					System.out.println("Employee Part Time");
-					dailywage = WAGE_PER_HOUR * PART_TIME_HOUR;
-					totalworkinghours1 = totalworkinghours1 + PART_TIME_HOUR;
+			switch (empcheck) {
+			case IS_FULL_TIME:
+				System.out.println("Employee Full Time");
+				dailywage = WAGE_PER_HOUR * FULL_TIME_HOUR;
+				totalworkinghours = totalworkinghours + FULL_TIME_HOUR;
+				break;
 
-					break;
+			case IS_PART_TIME:
+				System.out.println("Employee Part Time");
+				dailywage = WAGE_PER_HOUR * PART_TIME_HOUR;
+				totalworkinghours = totalworkinghours + PART_TIME_HOUR;
+				break;
 
-				default:
-					System.out.println("Employee is Absent");
+			default:
+				System.out.println("Employee is Absent");
 
-				}
-
-				totalwage1 = totalwage1 + dailywage;
-				System.out.println("Daly Wage => " + dailywage);
-				day++;
 			}
-
-			System.out.println("Total wage=>" + totalwage1);
-			System.out.println("Total working hours=>" + totalworkinghours1);
+			tatolwage = tatolwage + dailywage;
+			System.out.println("Daly Wage => " + day + " =>" + dailywage);
+			day++;
 		}
+		System.out.println("Total wage=>" + tatolwage);
+		System.out.println("Total working hours=>" + totalworkinghours);
+		return tatolwage;
+
+	}
+
+	@Override
+	public String toString() {
+		return "EmployeeWage{" + "COMPANY_NAME='" + COMPONY_NAME + '\'' + ",total_wage=" + total_wage + '}';
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("welcome to Employee Wage");
+		EmployeeWage dmart = new EmployeeWage("D-Mart", 20, 1, 10);
+		EmployeeWage nokia = new EmployeeWage("NOKIA", 15, 2, 10);
+		dmart.computeEmpWage();
+		System.out.println(dmart);
+		nokia.computeEmpWage();
+		System.out.println(nokia);
+
 	}
 }
